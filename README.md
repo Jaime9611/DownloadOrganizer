dd1cf21e4d51d1625eca2dc542d977b2fe2885ad
Jaime9611 <jaimeescobar9611@gmail.com>
2020-02-25 17:33:07 -0500

    Ready to submit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# DownloadOrganizer
+# Download Organizer
+
+Script to organize the files in the download directory by categories, using the **pathlib** module of the **Python Standard Library**.
--- directories.py
+++ directories.py
@@ -2,6 +2,9 @@ from pathlib import Path
 
 
 class Downloads:
+    """Class that represents the download directory and its organization."""
+
+    # Categories' names and the type of files they will contain.
     categories = {
         'Installers': ['.exe', '.msi'],
         'Images': ['.jpg'],
@@ -10,14 +13,38 @@ class Downloads:
     }
 
     def __init__(self, route):
-        self.route = Path(r'C:\Users\Jaime\Downloads')
+        """
+        Initialize the directory path, the files it contains and create the new 
+        directories for each category.
+        """
+
+        self.route = Path(route)
         self.files = self._search_files()
         self.dirs = self._make_dirs()
 
+    def organize(self):
+        """Organize the files in their respective category"""
+
+        for category, suffixes in self.categories.items():
+            for item in self.files[:]:
+                if item.suffix in suffixes:
+                    self._move_file(category, item)
+                    self.files.remove(item)
+
+        # ask for any uncategorized file
+        if self.files:
+            for item in self.files:
+                self._move_file('Others', item)
+
     def _search_files(self):
+        """Find out what files are in the folder and return a list of them."""
         return [ x for x in self.route.iterdir() if x.is_file() ]
 
     def _make_dirs(self):
+        """
+        Create the directories for each category, and return a dict with the name and its path.
+        """
+
         dirs = { name: self.route / name for name in self.categories }
 
         for route in dirs.values():
@@ -31,6 +58,8 @@ class Downloads:
         return dirs
 
     def _move_file(self, category, item):
+        """Move the file to the specified category."""
+
         if category == 'Others':
             target = self.route / category / item.name
         else:
@@ -38,19 +67,3 @@ class Downloads:
 
         if not target.exists():
             item.replace(target)
-
-    def organize(self):
-        for category, suffixes in self.categories.items():
-            for item in self.files[:]:
-                if item.suffix in suffixes:
-                    self._move_file(category, item)
-                    self.files.remove(item)
-
-        # ask for any uncategorized file
-        if self.files:
-            for item in self.files:
-                self._move_file('Others', item)
-
-
-if __name__ == '__main__':
-    d = Downloads('..a')
